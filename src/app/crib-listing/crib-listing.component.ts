2c97cd65a1ebcd9b3de76c43e67a755052e9af7b
H@c_git <harishchil679@gmail.com>
2017-11-15 12:10:24 -0600

    Add files via upload

---
+++ src/app/crib-listing/crib-listing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-crib-listing',
+  templateUrl: './crib-listing.component.html',
+  styleUrls: ['./crib-listing.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CribListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
