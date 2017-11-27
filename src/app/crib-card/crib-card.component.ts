7d2d3a3e0ef4d5d31172fc7bf8d575334e9733fc
H@c_git <harishchil679@gmail.com>
2017-11-26 23:32:36 -0600

    Update crib-card.component.ts

--- src/app/crib-card/crib-card.component.ts
+++ src/app/crib-card/crib-card.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit, ViewEncapsulation, Input} from '@angular/core';
 })
 export class CribCardComponent implements OnInit {
 
-	@Input('crib') crib:any;
-
+	@Input('crib') crib:any; //@Input('crib') 	     crib:any;
+			//		     |
+			//	 inputing property value to local property	
+			// we are inputing a property value('crib') to local property called crib and asssigning type as any.
+			//i.e., any given value passed as property value will be inputed to local property as ANY in out view (crib-card.component.html) which is crib.price, crib.type etc.
   constructor() { }
 
   ngOnInit() {
