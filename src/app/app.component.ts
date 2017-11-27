5ed66946c6200cd085728350588d58b600172b52
H@c_git <harishchil679@gmail.com>
2017-11-26 21:27:37 -0600

    understanding and assigning the data
    --> selector: 'app-root '      // this is a tag in index.html
    --> templateUrl: './app.component.html'   //views that are used by the components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({ //decorator
+  selector: 'app-root', // app-root is a tag in index.html
+  templateUrl: './app.component.html', //views that are used by the components.
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
