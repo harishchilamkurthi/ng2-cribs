214c1dbe20d4b34b50fdd35ffc0e3d8dacdc5be2
H@c_git <harishchil679@gmail.com>
2017-12-11 15:36:06 -0600

    Update crib-listing.component.ts

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -27,7 +27,7 @@ export class CribListingComponent implements OnInit { //
 	      .map(res=>res.json())
 	  //http request goes out, we get request from data/cribs.json file and what ever comes back will be MAPped to json file and the result will be SUBSCRIBE.
 	      .subscribe( 
-		  data => console.log(data) 
+		  data => console.log(data) ;
 	  );
 	  }
 
