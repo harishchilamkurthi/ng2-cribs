6ea99953dcd025a66962767c41058a74f78d79f1
H@c_git <harishchil679@gmail.com>
2017-12-11 16:52:12 -0600

    deleting data from ngOnInit vs service
    deleting the data from ngOnInit and using the newly created service.
    we dont need MAP any longer because we are not using it any more, we are getting the data directly from service and service has MAP,

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { Http } from '@angular/http'; //imports angular http requests.
 import 'rxjs/add/operator/map'; //imports from rxjs library.
 import { CribsService } from './..services/cribs.service'; //since the url is an injectable service, we need to inject it before we use it.
-
+							   //we are using one of the local componenets from this service.
 @Component({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
@@ -27,10 +27,10 @@ export class CribListingComponent implements OnInit { //
   ngOnInit() {
 	  //this.http.get('data/cribs.json'); // we are using cribs.json file for data and using "get" keyword.
 	  //this.http.get('data/cribs.json') //instead of using http request drectly, lets use CribsService.
-	    this.cribsService.getAllCribs() //this gives more access to subscribe.
-	      .map(res=>res.json())
+	    //  .map(res=>res.json())
 	  //http request goes out, we get request from data/cribs.json file and what ever comes back will be MAPped to json file and the result will be SUBSCRIBE.
-	      .subscribe( 
+	   this.cribService.getAllCribs() //this gives more access to subscribe.
+	       .subscribe( 
 		  data => this.cribs = data, //cribs is the local property which we used earlier.
 		  error => this.error = error.statusText //error is local property
 	  );
