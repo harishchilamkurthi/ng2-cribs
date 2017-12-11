4b75e2460c553be390be7321075f0c6d4bcc3739
H@c_git <harishchil679@gmail.com>
2017-12-11 16:46:31 -0600

    using services instead of using http req directly

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 //removing the above import because the ts fil eis converted to json file.
 import { Http } from '@angular/http'; //imports angular http requests.
 import 'rxjs/add/operator/map'; //imports from rxjs library.
+import { CribsService } from './..services/cribs.service'; //since the url is an injectable service, we need to inject it before we use it.
 
 @Component({
   selector: 'app-crib-listing',
@@ -18,12 +19,15 @@ export class CribListingComponent implements OnInit { //
 	
 	@Input('crib') crib;
 
-  // when a http request is used, we need to inject it to component which will make use of implementation that angular givesus for injecting dependencies which is done in constructor.	
-  constructor(private http : Http ) { } //gives us private instance of http and we can call by using this keyword.
-
+  // when a http request is used, we need to inject it to component which will make use of implementation that angular gives us for injecting dependencies which is done in constructor.	
+  //constructor(private http : Http ) { } //gives us private instance of http and we can call by using this keyword.
+  constructor(private http : Http, private cribService: CribsService ) { // we are privately injecting.
+  }
+	
   ngOnInit() {
 	  //this.http.get('data/cribs.json'); // we are using cribs.json file for data and using "get" keyword.
-	  this.http.get('data/cribs.json')
+	  //this.http.get('data/cribs.json') //instead of using http request drectly, lets use CribsService.
+	    this.cribsService.getAllCribs() //this gives more access to subscribe.
 	      .map(res=>res.json())
 	  //http request goes out, we get request from data/cribs.json file and what ever comes back will be MAPped to json file and the result will be SUBSCRIBE.
 	      .subscribe( 
