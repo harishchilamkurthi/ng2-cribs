93b15367b9ebca44de28fb137577bb373d9fd300
H@c_git <harishchil679@gmail.com>
2017-12-11 12:17:45 -0600

    Update crib-card.component.ts

--- src/app/crib-card/crib-card.component.ts
+++ src/app/crib-card/crib-card.component.ts
@@ -17,8 +17,9 @@ export class CribCardComponent implements OnInit {
 			//i.e., any given value passed as property value will be inputed to local property as ANY in out view (crib-card.component.html) which is crib.price, crib.type etc.
   			
 			//now the entire application is running on crib-card.component.ts and can be resued anywhere in the applciation.
+			// it still shows error 
 
-	@Input('crib') crib:Crib; //crib is local property and Crib is interface.
+	@Input('crib') crib:Crib; //crib is local property abide by Crib interface.
 	
   constructor() { }
  //lets assume that we dont knw what is going on with the values, like what values each item has, lets see if we want to use or get the number of bedrooms or kitchens,
