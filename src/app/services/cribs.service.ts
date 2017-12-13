53e231a1909c3deedfefe97aaba555f2b9554dad
H@c_git <harishchil679@gmail.com>
2017-12-12 20:33:25 -0600

    Update cribs.service.ts

--- src/app/services/cribs.service.ts
+++ src/app/services/cribs.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http'; //imports http class from angular core.
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map'; //importing the map and other functions from rxjs library.
-import {Subject} from 'rxjs/Subject';
+import {Subject} from 'rxjs/Subject'; //imports subject from rxjs.
 
 @Injectable()
 export class CribsService {
 
   public newCribSubject = new Subject<any>();
 
-  constructor(private http: Http) { //injecting http class
+    constructor(private http: Http) { //injecting http class
   }
 
   getAllCribs() {
@@ -18,7 +18,13 @@ export class CribsService {
                .map(res => res.json());
   }
 
-  addCrib() { //is the method used to communicate between the components
+  addCrib(data){ //is the method used to communicate between the components
+    this.newCribSubject.next(data);
+    //when the data is coming from addCrib(); when ever the button is clicked and the data is called, sent it through the subject.
+    //if the observed data comes over the period of time, always something comes next.
+    data.image = 'default-crib';
+    //image property that we have should be default because we dont have any specific image to insert for this listing.
   }
+  
 
 }
