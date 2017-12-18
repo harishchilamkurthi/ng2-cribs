bc632188566eff986db1b786f0e612db1802c518
H@c_git <harishchil679@gmail.com>
2017-12-18 13:02:21 -0600

    Update sort-by.pipe.ts

--- src/app/pipes/sort-by.pipe.ts
+++ src/app/pipes/sort-by.pipe.ts
@@ -1,25 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
+@Pipe({ //marking the class
   name: 'sortBy'
 })
 export class SortByPipe implements PipeTransform {
+             //sortby is angular pipe which attaches some meta data.
+        //wen we create a pipe, we need to create a class called pipetransform which says that needs to have transform() within the pipe which is responsible to do the pipe job.
+        //in our case the pipe is meant to sort the values and logic is going to be in transform().
 
-  transform(array: Array<any>, args?: any): any {
-
-    if (array) {
-
-      let sortField = args[0]; // the field we want to sort by
-      let sortDirection = args[1];
-
-      let modifier = 1;
-
-      if (sortDirection == "desc") {
-        modifier = -1;
-      }
-
-      array.sort((a: any, b: any) => {
-
+  transform(array: Array<any>, args?):  {
+    //lets check to see if we have an array.
+    //we dont want this transform to run if there is no data that comes into it. for that
+    
+    if(array){
+      let sortField = args[0];
+      array.sort((a:any, b:any) => {
+                //compare function, needs 2 parameters with type any which represents 2 elements in an array.
+       
+      //CONDITION: compare function has 2 return 1 of 3 numbers
+          //return 1      i.e., a>b
+          //return -1      i.e., a<b
+          //return 0      i.e., a=b
         if (a[sortField] < b[sortField]) {
           return -1 * modifier;
         } else if (a[sortField] > b[sortField]) {
@@ -29,8 +30,10 @@ export class SortByPipe implements PipeTransform {
         }
       });
 
-      return array;
+    return array;
     }
+
+    
   }
 
 }
