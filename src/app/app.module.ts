144956f7d42d613f71201df75af2b5213db8efe0
H@c_git <harishchil679@gmail.com>
2017-12-18 12:06:03 -0600

    providing the service
    Note: when a new service is created, it needs to be imported and provided in app.module.ts.
    adding the service in import and provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CribsService} from './services/cribs.service'; //CribsService will be u
 import { CribListingComponent } from './crib-listing/crib-listing.component';
 import { CribCardComponent } from './crib-card/crib-card.component';
 import { FormModule } from '@angular/forms';
+import { UtilService } from './services/util.service';
 
 @NgModule({ //point 1
   declarations: [  //whenever we create a new component, we declare here in decorator.
@@ -20,7 +21,7 @@ import { FormModule } from '@angular/forms';
     FormModule
   ],
   //providers: [], // any injectable services that we create
-  providers: [CribsService], //service that is created should be provided.
+  providers: [CribsService, UtilService], //service that is created should be provided.
   bootstrap: [AppComponent] // main component is bootstrapped
 })
 export class AppModule { }
