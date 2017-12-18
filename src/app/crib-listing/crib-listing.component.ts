8ad1e54316eda4cb504459802a78b73862bb064f
H@c_git <harishchil679@gmail.com>
2017-12-18 11:54:46 -0600

    adding sort field
    just added a sortfield, we need to use a service to capitalize the first letter before it shows on the screen.

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -15,13 +15,24 @@ export class CribListingComponent implements OnInit { //
 	//cribs: Array<any> = cribs; // we are assigning the type as any and assigning it to cribs data. here crib is a member and cribs(red) is a local memeber.
 	cribs: Array<any> = [];
 	//the cribs injection file is no more. it has been renamed to json file.
-	
+	error: string = '';
+	 sortField: string = 'price';
+	 sortDirection: string = 'asc';
+	 sortFields: Array<string> = [
+	    'address',
+	    'area',
+	    'bathrooms',
+	    'bedrooms',
+	    'price',
+	    'type'
+	 ];
 	
 	@Input('crib') crib;
 
   // when a http request is used, we need to inject it to component which will make use of implementation that angular gives us for injecting dependencies which is done in constructor.	
   //constructor(private http : Http ) { } //gives us private instance of http and we can call by using this keyword.
-  constructor(private http : Http, private cribService: CribsService ) { // we are privately injecting.
+  constructor(private http : Http, 
+	      private cribService: CribsService ) { // we are privately injecting.
   }
 	
   ngOnInit() {
